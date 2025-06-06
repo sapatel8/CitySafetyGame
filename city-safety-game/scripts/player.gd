11cefd16e4d169831ad8827c2ab21fecb89bc59f
Erica Duh <ericaduh1@gmail.com>
2025-06-06 10:48:03 +0200

    Basic Player, Stranger, Killzone, Main Scenes

---
+++ city-safety-game/scripts/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var direction: Vector2 = Vector2.ZERO
+var idle_direction: Vector2 = Vector2(1,0)
+var speed: int = 100
+@onready var player: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("walking_left", "walking_right", "walking_up", "walking_down")
+	velocity = direction * speed
+	move_and_slide()
+	
+	if direction.length() > 0: 
+		idle_direction = direction
+		play_walking_animation(direction)
+	else: 
+		play_idle_animation(direction)
+	
+func play_walking_animation(direction): 
+	if direction.x > 0: 
+		player.play("walking_right")
+	elif direction.x < 0: 
+		player.play("walking_left")
+	elif direction.y < 0: 
+		player.play("walking_up")
+	elif direction.y > 0: 
+		player.play("walking_down")
+
+func play_idle_animation(direction): 
+	if idle_direction.x > 0: 
+		player.play("idle_right")
+	elif idle_direction.x < 0: 
+		player.play("idle_left")
+	elif idle_direction.y < 0: 
+		player.play("idle_up")
+	elif idle_direction.y > 0: 
+		player.play("idle_down")
---
+++ city-safety-game/scripts/stranger.gd
@@ -0,0 +1,62 @@
+extends Area2D
+
+var speed: int = 100
+var previous_position: Vector2
+var direction: Vector2 = Vector2.ZERO
+var idle_direction: Vector2 = Vector2.RIGHT
+@onready var stranger: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Stranger Danger!")
+
+func _ready():
+	previous_position = position
+
+func _physics_process(delta: float) -> void:
+	# Detect actual movement direction based on position delta
+	var movement = position - previous_position
+
+	if movement.length() > 0:
+		direction = movement.normalized()
+		idle_direction = direction
+		play_walking_animation(direction)
+	else:
+		play_idle_animation(idle_direction)
+
+	previous_position = position  # Update for next frame
+
+func play_walking_animation(dir: Vector2) -> void:
+	# If horizontal movement of character is dominant
+	if abs(dir.x) > abs(dir.y):
+		# If character is moving right
+		if dir.x > 0:
+			stranger.play("walking_right")
+		# If character is moving left
+		else:
+			stranger.play("walking_left")
+	# If vertical movement of character is dominant
+	else:
+		# If character is moving upwards
+		if dir.y < 0:
+			stranger.play("walking_up")
+		# If character is moving downwards
+		else:
+			stranger.play("walking_down")
+
+func play_idle_animation(dir: Vector2) -> void:
+	# If horizontal movement of character is dominant
+	if abs(dir.x) > abs(dir.y):
+		# If character is moving right
+		if dir.x > 0:
+			stranger.play("idle_right")
+		# If character is moving left
+		else:
+			stranger.play("idle_left")
+	# If vertical movement of character is dominant
+	else:
+		# If character is moving upwards
+		if dir.y < 0:
+			stranger.play("idle_up")
+		# If character is moving downwards
+		else:
+			stranger.play("idle_down")
