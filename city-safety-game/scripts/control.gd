9701e603dd890fe36d5bccce8da7eead46a75ada
sapatel8 <127165176+sapatel8@users.noreply.github.com>
2025-06-10 10:35:16 +0200

    created main menu

---
+++ city-safety-game/scripts/control.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene("res://scenes/main.tscn")
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
