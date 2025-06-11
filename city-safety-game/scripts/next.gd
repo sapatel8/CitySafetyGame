53665de8d7ebdcf33e2a3105a34425ab68ef4e05
Erica Duh <ericaduh1@gmail.com>
2025-06-11 14:29:05 +0200

    Revert "Merge branch 'main' of https://github.com/sapatel8/CitySafetyGame"
    This reverts commit 3b593e788cd966dbef1f45a2e4148959c5e9300b, reversing
    changes made to 8390c7d0723decebaa48bbe5d57155366bf65b96.

--- city-safety-game/scripts/next.gd
+++ city-safety-game/scripts/next.gd
@@ -15,13 +15,9 @@ func _on_body_entered(body: Node2D) -> void:
 			# Convert name of scene into its integer number only, then add 1
 			next_level_number = current_scene_file.to_int() + 1
 		print(next_level_number)
-		
-		if next_level_number == 5: 
-			get_tree().change_scene_to_file("res://scenes/endScreen.tscn")
-		else: 
-			# Create path using new level number
-			var next_level_path = FILE_BEGIN + str(next_level_number) + FILE_END
-			print(next_level_path)
-			get_tree().change_scene_to_file(next_level_path)
+		# Create path using new level number
+		var next_level_path = FILE_BEGIN + str(next_level_number) + FILE_END
+		print(next_level_path)
+		get_tree().change_scene_to_file(next_level_path)
 		
 		
