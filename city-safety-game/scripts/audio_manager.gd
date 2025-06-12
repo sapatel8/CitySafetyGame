5f44d6425bcd36564015eb24e8fc0dca7bc89fc0
sapatel8 <127165176+sapatel8@users.noreply.github.com>
2025-06-12 11:00:37 +0200

    looping music

--- city-safety-game/scripts/audio_manager.gd
+++ city-safety-game/scripts/audio_manager.gd
@@ -6,6 +6,7 @@ func _ready():
 	music_player.autoplay = true
 	add_child(music_player)
 	music_player.volume_db = -10
-	music_player.stream = preload("res://assets/sounds/best-game-console-301284.mp3")
-	#music_player.loop = true
+	var stream = preload("res://assets/sounds/best-game-console-301284.mp3")
+	stream.loop = true
+	music_player.stream = stream
 	music_player.play()
